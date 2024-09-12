b4f86957163b632389b2cc95b3cbf43befca4c9e
CodeElk <chaomengop@163.com>
2024-09-12 08:35:38 +0800

    3.traingle

--- cmake-build-debug/CMakeFiles/tinyRender.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for tinyRender.
-# This may be replaced when dependencies are built.
--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 
+#include "model.h"
 #include "tgaimage.h"
 using namespace std;
 const TGAColor white = TGAColor(255, 255, 255, 255);
@@ -37,16 +38,51 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
 	}
 }
 
+void triangle(Vec2i t0, Vec2i t1, Vec2i t2, TGAImage &image, TGAColor color) {
+	//填充三角形
+	//把三角形分成两个三角形，分别填充
+	//看是不是空心三角形，如果是，就画线，如果不是，就画填充
+	if(t0.x == t1.x && t0.y == t2.y) {return;}
+	//根据y坐标排序
+	if(t0.y > t1.y) {std::swap(t0, t1);}
+	if(t0.y > t2.y) {std::swap(t0, t2);}
+	if(t1.y > t2.y) {std::swap(t1, t2);}
+	//画线
+	for(int y=t0.y; y<=t2.y; y++) {
+		bool isTop = y>t1.y;
 
+	}
+}
 int main(int argc, char** argv) {
-	TGAImage image(400, 400, TGAImage::RGB);
+	int width = 400;
+	int height = 400;
+	TGAImage image(width, height, TGAImage::RGB);
 
+	Model* model = new Model("../obj/african_head.obj"); //代码方式构造model
 
-	// image.flip_horizontally(); // i want to have the y-axis pointing upwards
-	line(13, 40, 80, 40, image, white); //线段A
-	line(20, 13, 40, 80, image, red); //线段B
-	line(80, 40, 13, 20, image, red);//线段C
-	image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
+	//nfaces()返回模型的面数
+	for (int i = 0; i < model->nfaces(); i++) {
+
+		vector<int> face = model->face(i);//获得第i个面的顶点索引
+		for (int j = 0; j < 3; j++) {
+
+			Vec3f vert1 = model->vert(face[j]);//获得第j个顶点的xyz坐标
+			Vec3f vert2 = model->vert(face[(j+1)%3]);//获得第j+1个顶点的xyz坐标
+
+			//对vert1和vert2进行屏幕坐标转换
+			float x1 = (vert1.x + 1) * width / 2;
+			float y1 = (vert1.y + 1) * height / 2;
+			float x2 = (vert2.x + 1) * width / 2;
+			float y2 = (vert2.y + 1) * height / 2;
+			//调用bresenham算法绘制直线
+			line(x1, y1, x2, y2, image, red);
+
+		}
+
+	}
+
+	// line(100, 100, 300, 300, image, red);
+	image.flip_vertically();
 	image.write_tga_file("output.tga");
 	std::cout << "Image saved to output.tga" << std::endl;
 	return 0;
